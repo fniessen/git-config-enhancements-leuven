1f4f428afc53e7815f8db546fe3ac83c277e7c95
Fabrice Niessen <fniessen@pirilampo.org>
2023-04-14 05:01:02 +0200

    Refactor code

--- bin/bitbucket-list-issues.R
+++ bin/bitbucket-list-issues.R
@@ -3,22 +3,22 @@
 library(httr)
 library(jsonlite)
 
-## Set up the BitBucket API endpoint and credentials.
+## Define BitBucket API endpoint and credentials.
 c.username.slug <- "mcpflow/pflow"
 c.base.url <- paste0("https://api.bitbucket.org/2.0/repositories/", c.username.slug, "/issues")
 c.username <- "mcpflow"
 c.pw <- "XXX"
 
-## Function to list issues.
+## Define function to list issues.
 list_issues <- function() {
   response <- httr::GET(c.base.url, authenticate(c.username, c.pw))
   httr::stop_for_status(response)
-  content(response, as = "text", encoding = "UTF-8")
+  issues_json <- content(response, encoding = "UTF-8")
+  fromJSON(issues_json)                 # Parse the JSON response.
 }
 
-## Call the list_issues function and parse the JSON response.
-issues_json <- list_issues()
-issues <- jsonlite::fromJSON(issues_json)
+## Call the list_issues function.
+issues <- list_issues()
 
 ## Print the parsed JSON data.
 print(issues)
